369e326bc31047f65cad1f063605e826d0f60ec6
PawelKoralewicz <116287573+PawelKoralewicz@users.noreply.github.com>
2023-03-27 09:29:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const btr = document.getElementById('battery-status');
+const btr = document.getElementById('level');
 
 if (navigator.getBattery) {
     navigator.getBattery()
     .then(function(battery) {
         var batteryLevel = battery.level * 100;
-        btr.innerHTML = `battery level: ${batteryLevel}%`;
-        console.log(batteryLevel);
+        btr.innerHTML = `${batteryLevel}%`;
+        btr.style.width = `${batteryLevel}%`;
+        if (batteryLevel >= 50) {
+            btr.style.background = '#25ee25';
+        } else if (batteryLevel > 20 && batteryLevel < 50) {
+            btr.style.background = 'rgb(255, 217, 0)';
+        } else if (batteryLevel <= 20) {
+            btr.style.background = 'rgb(240, 7, 7)';
+        }
     })
     .catch(function(e) {
         console.error(e);
